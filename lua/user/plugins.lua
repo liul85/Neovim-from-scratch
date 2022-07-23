04847d4312f4814169699d457f54552df76fee2c
liul85 <liul85@gmail.com>
2022-07-23 18:50:04 +1000

    Added rust-tools

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -92,6 +92,12 @@ return packer.startup(function(use)
 	-- Git
 	use({ "lewis6991/gitsigns.nvim", commit = "c18e016864c92ecf9775abea1baaa161c28082c3" })
 
+  --Rust
+  use("simrat39/rust-tools.nvim")
+
+  --Gruvbox
+  use("ellisonleao/gruvbox.nvim")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
