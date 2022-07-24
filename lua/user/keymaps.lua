2a143ac7494c140701cd0106a2a88189b42e476c
liul85 <liul85@gmail.com>
2022-07-24 19:46:49 +1000

    Added troubles

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -60,6 +60,9 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
+-- trouble plugin
+keymap("n", "<leader>r", "<cmd>TroubleToggle<cr>", opts)
+
 -- Terminal --
 -- Better terminal navigation
 -- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -96,6 +96,8 @@ return packer.startup(function(use)
   --Rust
   use("simrat39/rust-tools.nvim")
 
+  use({"folke/trouble.nvim", requires = "kyazdani42/nvim-web-devicons"})
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
